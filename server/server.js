9a7a6a4d54a784670655a79fdc8b5b2a7bb945b7
elhemriilhame-ops <elhemriilhame@gmail.com>
2026-03-16 11:56:54 +0000

    connecting database

--- server/server.js
+++ server/server.js
@@ -4,22 +4,33 @@ const cors = require('cors');
 const connectDB = require('./src/config/db');
 
 // Load env vars
-// dotenv.config();
-// \n\n// Connect to database
-// connectDB();
-// \n\nconst app = express();
-// \n\n// Body parser
-// app.use(express.json());
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
 // Enable CORS
-// \napp.use(cors());
+app.use(cors());
+
 // Static folder for uploads
-// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
+
 // Mount basic route for testing
-// app.get('/', (req, res) => {\n  res.send('API is running...');
-// \n});\n\n// We will mount our routes here
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// We will mount our routes here
 // app.use('/api/auth', require('./src/routes/auth'));
 // app.use('/api/users', require('./src/routes/users'));
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+});
